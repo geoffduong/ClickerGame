be5d3ec0d636b5dbbad6dbb2c4d10183c8450dbc
mrricearoni <duongg4848@gmail.com>
2017-03-19 22:54:00 -0400

    Added BottomBar

--- app/src/main/java/com/example/geoffduong/clickergame/MainActivity.java
+++ app/src/main/java/com/example/geoffduong/clickergame/MainActivity.java
@@ -26,15 +26,6 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         counterText = (TextView) findViewById(R.id.counterText);
         counterText.setText(Integer.toString(count));
         counter = new CounterThread("counterThread");
@@ -72,6 +63,7 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    // Counter Thread
     class CounterThread implements Runnable {
         private Thread thread;
         private String threadName;
